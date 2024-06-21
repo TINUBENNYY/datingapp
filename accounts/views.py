b42c5e708fa7155ee8c325fc42134a6282a723be
tinu benny <your-tinubenny0526@gmail.com>
2024-06-21 19:37:00 +0530

    auth commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import User
+from .forms import LoginFrom,UserCreationForm
 # Create your views here.
+
+def user_register(request):
+    context = {}
+    if request.method == 'GET':
+        context['form'] = UserCreationForm()
+        return render(request, 'accounts/register.html', context)
+    elif request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            return redirect('login')
+        else:
+            messages.error(request, 'Registration failed')
+            context['form'] = form
+            return render(request, 'accounts/register.html', context)
+
+
+def user_login(request):
+    context = {}
+    if request.method == 'GET':
+        context['form'] = LoginFrom()
+        return render(request, 'accounts/login.html', context)
+    elif request.method == 'POST':
+        form = LoginFrom(data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse(f'user login successful 9username : {username})')
+            else:
+                return HttpResponse('Login failed')
+            
+         # If the form is not valid.
+        context['form'] = form
+        return render(request, 'accounts/login.html', context)
--- datingapp/settings.py
+++ datingapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +79,11 @@ WSGI_APPLICATION = 'datingapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # Replace with your engine
+        'NAME': 'mydatabase.db',  # Replace with your database name
     }
 }
+
 
 
 # Password validation
@@ -116,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'accounts/static'),
+]
+
+# Where to collect static files in production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
